5b850e46a1a615c28dee043921e83e706c7be979
Gerard <gerarddoncel41@gmail.com>
2024-05-22 23:53:11 +0200

    genearl fixes, hero layout changed, font size changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,13 +5,23 @@ export default {
 		extend: {
 			animation: {
 			  'infinite-scroll': 'infinite-scroll 15s linear infinite',
+			  'zoomIn': 'zoomIn 0.2s ease-in forwards',
+			  'zoomOut': 'zoomOut 0.2s ease-out forwards',
 			},
 			keyframes: {
 			  'infinite-scroll': {
 				from: { transform: 'translateX(0)' },
 				to: { transform: 'translateX(-100%)' },
-			  }
-			}                    
+			  },
+			  'zoomIn': {
+				'0%': { transform: 'scale(1)' },
+				'100%': { transform: 'scale(1.05)' },
+			  },
+			  'zoomOut': {
+				'0%': { transform: 'scale(1.05)' },
+				'100%': { transform: 'scale(1)' },
+			  },
+			}                
 		},
 	},
 	plugins: [
